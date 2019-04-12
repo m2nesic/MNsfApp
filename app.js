42ebe8cce84e464c5d9642b5d0c1f8bdd73494eb
m2nesic <michaelnesic@gmail.com>
2019-04-12 09:41:03 -0400

    reset all

--- app.js
+++ app.js
@@ -7,11 +7,9 @@ var app = express();
 app.set('view engine', 'hbs');
 app.enable('trust proxy');
 
-//hardcoded key and secret
 process.env['CONSUMER_KEY'] = "3MVG9ZF4bs_.MKuht6utTb9K3jHsVAS.7uRPWNUJTQK7v324lBG10qgrFif0V_vH7.smSFzpRENbwqxkBqtvu" 
 process.env['CONSUMER_SECRET'] = "10037C4741B9F4619E34B71655BFA02D218BE158AC8EC053E6F6B740183C9E48"
 
-C
 function isSetup() {
   return (process.env.CONSUMER_KEY != null) && (process.env.CONSUMER_SECRET != null);
 }
@@ -34,12 +32,11 @@ app.get('/', function(req, res) {
     });
 
     if (req.query.code !== undefined) {
+      // authenticated
       org.authenticate(req.query, function(err) {
         if (!err) {
-          // nforce SOQL query 
-          org.query({ query: 'SELECT Name, Genre__c, Game_Studio__r.Name , Release_Date__c,TextPicture__c FROM Game__c ORDER BY Release_Date__c ASC' }, function(err, results) {
+          org.query({ query: 'SELECT id, name, type, industry, rating FROM Account' }, function(err, results) {
             if (!err) {
-              // Render the json objects by having the handlebar tags recieve them
               res.render('index', {records: results.records});
             }
             else {
